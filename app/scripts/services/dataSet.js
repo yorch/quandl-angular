0af0eefc8b55dcb414cc05f2dab108c52cd1866b
Jorge Barnaby <jorge.barnaby@gmail.com>
2013-10-26 11:53:08 -0400

    dataSet Service updated to use new apiUrl service.

--- app/scripts/services/dataSet.js
+++ app/scripts/services/dataSet.js
@@ -4,9 +4,11 @@ angular.module('quantlTestApp')
   .service('dataSet', function($http, $log, apiUrl) {
     return {
       search: function(query, callback) {
-        var section = 'datasets'
-        var params = 'query=' + query;
-        var url = apiUrl.build_url(section, params);
+        var section = 'datasets';
+        var params = {
+          'query': query
+        };
+        var url = apiUrl.buildUrl(section, params);
 
         $log.info('Searching DataSets', query, 'at', url);
 
@@ -17,11 +19,17 @@ angular.module('quantlTestApp')
           });
       },
       get: function(name, callback) {
+        this.get_with_filter(name, null, null, callback);
+      },
+      get_with_filter: function(name, startDate, endDate, callback) {
         var section = 'datasets/' + name;
-        var url = apiUrl.build_url(section);
+        var params = {
+          'trim_start': startDate,
+          'trim_end': endDate
+        };
+        var url = apiUrl.buildUrl(section, params)
 
         $log.info('Getting DataSet', name, 'at', url);
-
         $http.get(url)
           .success(function(data) {
             $log.info('DataSet Success', name, 'result', data);
