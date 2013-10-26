e7cb1114f25211d0a8b32015393b820ec6a7b70b
Jorge Barnaby <jorge.barnaby@gmail.com>
2013-10-26 11:52:31 -0400

    apiUrl service improved (with tests).

--- app/scripts/services/apiUrl.js
+++ app/scripts/services/apiUrl.js
@@ -1,13 +1,26 @@
 'use strict';
 
 angular.module('quantlTestApp')
-  .service('apiUrl', function (appSettings) {
+  .service('apiUrl', function(appSettings) {
     return {
-      build_url: function(section, params) {
-        var auth_token = 'auth_token=' + appSettings.apiToken;
-        var url = appSettings.apiBaseUrl + section + '.' + appSettings.apiExt + '?' +
-          auth_token + (params ? '&' + params : '');
-        return url;
+      toQueryString: function(params) {
+        var parts = [];
+        params = params || {};
+        for (var i in params) {
+          if (params.hasOwnProperty(i)) {
+            if (params[i]) { // Only include non empty params
+              parts.push(encodeURIComponent(i) + "=" + encodeURIComponent(params[i]));
+            }
+          }
+        }
+        return parts.join("&");
+      },
+      buildUrl: function(section, params) {
+        var auth_token = {
+          'auth_token': appSettings.apiToken
+        };
+        angular.extend(params, auth_token);
+        return appSettings.apiBaseUrl + section + '.' + appSettings.apiExt + '?' + this.toQueryString(params);
       }
     }
   });
