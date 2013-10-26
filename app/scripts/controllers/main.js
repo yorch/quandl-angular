8e1c4e85c8e77a1c0ca4edc4faec6a9a60f13541
Jorge Barnaby <jorge.barnaby@gmail.com>
2013-10-26 11:54:14 -0400

    MainCtrl functionality from original application (quantl-angular).

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('quantlTestApp')
-  .controller('MainCtrl', function ($scope, $log, dataSet) {
-    
+  .controller('MainCtrl', function($scope, $http, $log, appSettings, dataSet) {
+    $scope.model = {
+      dataSet: {},
+      filter: {
+        dataSet: 'BITCOIN/MTGOXUSD',
+        startDate: "2013-09-01",
+        endDate: "2013-12-31"
+      }
+    };
+
+    $scope.getDataSet = function() {
+      dataSet.get_with_filter($scope.model.filter.dataSet, $scope.model.filter.startDate, $scope.model.filter.endDate,
+        function(data) {
+          $scope.model.dataSet = data;
+        });
+    };
   });
