5c4ac1ca60df5427e23d11a0c2be40a28ccd4e36
Jorge Barnaby <jorge.barnaby@gmail.com>
2013-10-29 10:08:45 -0400

    Karma config updated.

--- karma.conf.js
+++ karma.conf.js
@@ -13,9 +13,11 @@ module.exports = function(config) {
     files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
@@ -45,7 +47,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['ChromeCanary'],
 
 
     // Continuous Integration mode
